46c6b436856284d6221b8b5d04f83c7e584fcb91
Eosellmay Li <2507917746@qq.com>
2026-04-14 03:37:25 +0800

    chore: autopublish 2026-04-13T19:38:21Z

---
+++ target/wasm32-unknown-unknown/release/deps/test_wasm.d
@@ -0,0 +1,5 @@
+/home/runner/work/eslint-config/eslint-config/target/wasm32-unknown-unknown/release/deps/test_wasm.d: packages/test_wasm/src/lib.rs
+
+/home/runner/work/eslint-config/eslint-config/target/wasm32-unknown-unknown/release/deps/test_wasm.wasm: packages/test_wasm/src/lib.rs
+
+packages/test_wasm/src/lib.rs:
---
+++ target/wasm32-unknown-unknown/release/test_wasm.d
@@ -0,0 +1,1 @@
+/home/runner/work/eslint-config/eslint-config/target/wasm32-unknown-unknown/release/test_wasm.wasm: /home/runner/work/eslint-config/eslint-config/packages/test_wasm/src/lib.rs
